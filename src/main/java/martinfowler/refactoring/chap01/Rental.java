d3aae3e0b197aa4a9ee1cad0eae58ceada47f680
chulwoo.jang <chulwoon.jang@navercorp.com>
2020-01-02 13:55:12 +0900

    write sample code

---
+++ src/main/java/martinfowler/refactoring/chap01/Rental.java
@@ -0,0 +1,19 @@
+package martinfowler.refactoring.chap01;
+
+public class Rental {
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie movie, int daysRented) {
+		this.movie = movie;
+		this.daysRented = daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+}
