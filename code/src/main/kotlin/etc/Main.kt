60ca72fa89c2c3281eb7767697eaf3fdbd1be871
jangchulwoon <lusiue@gmail.com>
2022-03-03 19:31:34 +0900

    4장 정리

--- code/src/main/kotlin/etc/Main.kt
+++ code/src/main/kotlin/etc/Main.kt
@@ -1,5 +1,7 @@
 package etc
 
+import java.util.*
+
 fun main() {
     val collection = CollectionTest()
 
@@ -8,4 +10,19 @@ fun main() {
     collection.roList += "hello"
 
 
+}
+
+class PropertiesTest {
+    var millis: Long = 0
+
+    // backing field 없이 get / set 사용 시, field 가 정의되지 않는다.
+    var date: Date
+        get() {
+            // 함수로만 사용된다.
+            // print(field)
+            return Date(millis)
+        }
+        set(value) {
+            millis = value.time
+        }
 }
